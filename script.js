8e4bd492c70d23a2528a2977568f7c14e8e454cd
udin471 <udincok12345678990@gmail.com>
2026-07-04 06:58:31 +0700

    Add loading animation to boot text

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const bootText = document.querySelector("#boot p");
 
+let dots = 0;
+
+setInterval(() => {
+    dots++;
+
+    if (dots > 3) {
+        dots = 0;
+    }
+
+    bootText.textContent = "INITIALIZING" + ".".repeat(dots);
+}, 500);
