878a33696ecbb58d24e6badf57b569e283a5d2a6
udin471 <udincok12345678990@gmail.com>
2026-07-05 17:57:27 +0700

    Add mainStoryPage and mainStoryReturnBtn elements

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const enterBtn = document.getElementById("enterBtn");
 const menu = document.getElementById("menu");
 const missionPage = document.getElementById("missionPage");
 const returnBtn = document.getElementById("returnBtn");
-
+const mainStoryPage = document.getElementById("mainStoryPage");
+const mainStoryReturnBtn = document.getElementById("mainStoryReturnBtn");
 
 // ======================================
 // HTML VALIDATION
